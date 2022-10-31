e7631d159a309cdb83431ba4e601bc9018b41e94
easther <eastherao1@gmail.com>
2022-10-31 15:24:09 +0300

    Added all files

--- todolistapi/settings.py
+++ todolistapi/settings.py
@@ -14,13 +14,13 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@t6qu1^u!0y417uty_6+a$)r6d+%nz)e$@yi+#1u#p8mk59(1'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
